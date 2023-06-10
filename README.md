3ff5b8f09c3f61cb7532e6f36dbd65fc1bd6c92d
Emiliano Conti <econtirodriguez@outlook.com>
2023-06-10 17:49:07 -0300

    Cambio de Estructura

--- README.md
+++ README.md
@@ -9,3 +9,7 @@
 - Se puede correr la aplicacion mediante el siguiente comando:
     - ```uvicorn main:app --reload```
     - Para generar una nueva clave usar ```openssl rand -hex 32```
+
+Para correr el Docker usar:
+- ```docker build -t fastapi .```
+- ```docker run -d -p 8000:8000 fastapi```
---
+++ main.py
@@ -0,0 +1,39 @@
+from typing import Union, Annotated
+from fastapi import Depends, FastAPI, HTTPException, status
+from pydantic import BaseModel
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+
+from api.authentication import ACCESS_TOKEN_EXPIRE_MINUTES, ALGORITHM, SECRET_KEY, Token, TokenData, User, authenticate_user, create_access_token, get_current_user, get_user, oauth2_scheme
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)]
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
